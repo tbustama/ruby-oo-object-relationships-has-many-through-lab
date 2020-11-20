503aca0a877b1370e519c4e31bf253946379662b
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-19 16:47:51 -0800

    commit tree 9a36609c996ad27d5d54de01de34cd3f022cd7b6

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,24 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select{|appointment| appointment.doctor == self}
+    end
+
+    def patients
+        self.appointments.map{|appointment| appointment.patient}
+    end
+end
